34043d800d861236fef3cedb15f4b1d06ffc9c3b
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2026-01-30 19:02:06 +0100

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Services from './components/Services'
-import Process from './components/Process'
-import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="min-h-screen">
       <Navbar />
       <Hero />
-      <Services />
-      <Process />
-      <Contact />
       
       {/* Footer */}
       <footer className="bg-gray-900 text-white py-8 px-6">
